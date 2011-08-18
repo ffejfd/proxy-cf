c671844f4275e038f89e6a9579070195f81cd6a4
Jeff Lee <jafelee@gmail.com>
2011-08-18 17:35:56 +0800

    Use new api 'http.request' to replace the deprecated api 'http.createClient'

--- app.js
+++ app.js
@@ -1,20 +1,34 @@
 var http = require('http');
+var sys = require('sys');
+var url = require('url');
+
 http.createServer(function(request, response) {
-  var proxy = http.createClient(80, request.headers['host'])
-  var proxy_request = proxy.request(request.method, request.url, request.headers);
-  proxy_request.addListener('response', function (proxy_response) {
-    proxy_response.addListener('data', function(chunk) {
-      response.write(chunk, 'binary');
-    });
-    proxy_response.addListener('end', function() {
-      response.end();
-    });
-    response.writeHead(proxy_response.statusCode, proxy_response.headers);
-  });
-  request.addListener('data', function(chunk) {
-    proxy_request.write(chunk, 'binary');
-  });
-  request.addListener('end', function() {
-    proxy_request.end();
-  });
+	//TODO:very awful implementation. Maybe there is a better way to 
+	//transfer the request.
+	var h = url.parse('http://' + request.headers['host']);
+	var options = {
+		host: h['hostname'],
+		port: h['port'],
+		path: request.url,
+		method: request.method,
+		headers: request.headers
+	};
+	var proxy = http.request(options, function(proxy_response) {
+		proxy_response.addListener('data', function(chunk) {
+			response.write(chunk, 'binary');
+		});
+
+		proxy_response.addListener('end', function(){
+			response.end();
+		});
+		response.writeHead(proxy_response.statusCode, proxy_response.headers);
+	});
+	request.addListener('data', function(chunk){
+		proxy.write(chunk, 'binary');
+	});
+
+	request.addListener('end', function(){
+		proxy.end();
+	});
 }).listen(8080);
+console.log('Server running at port 8080');
